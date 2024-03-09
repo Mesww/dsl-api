39a0d541bca15b9659651c00d12a3bf10ba6bf88
Nathikan <6431501049@lamduan.mfu.ac.th>
2024-03-09 22:09:32 +0700

    backend 14.11

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const cors = require('cors');
-const port = 7777;
+const mysql = require('mysql2')
+require('dotenv').config()
+
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+})
+
+const port = 3000;
 
 var app = express();
 
 
-app.use(cors);
+app.use(cors());
 
 app.get('/HelloWorld',function (req,res,next) {
     res.json({msg:'HelloWorld'});
 });
+
+app.get('/attractions', function(req, res, next){
+    pool.query("SELECT * FROM user", function(err, rows, fields){
+        res.json(rows)
+    })
+})
 
 app.listen(port,function () {
     console.log(`Server listening port: ${port} `);
